204d77b70e7a703580eab8dee22233e1f1495ccc
kelbri10 <kelbri10@gmail.com>
2020-09-27 13:29:33 -0400

    added stringify/parse to post to push new note to the json file and rewrite it

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,5 +1,6 @@
 const express = require('express'); 
 const router = express.Router(); 
+const fs = require('fs'); 
 const notes = require('../db/db.json'); 
 
 //returns saved notes as JSON
@@ -9,7 +10,20 @@ router.get('/', (req, res) => {
 
 //saves note, adds it to the db.json file and return new note 
 router.post('/', (req, res) => {
-    res.send(req.body); 
+    
+    //use fs to read file and turn db.json to a array of strings 
+    const notes = JSON.stringify(db.json); 
+
+    //take the note and append the title and body as a new object to the 
+    //array 
+    notes.push(req.body); 
+    //return array to json and write file again, db.json
+    JSON.parse(notes); 
+
+    //rewrites notes to json file
+    fs.writeFile('db.json', notes, 'utf-8', () =>{
+         if (err) throw err; 
+    })
 })
 
 
